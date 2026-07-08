c7c7bec4dcb0d0dde35a326f90c34fa4c5776fef
Daniel <danielcorbe@gmail.com>
2026-07-08 00:48:41 -0300

    update favicon

--- wp-content/themes/nutsclub/header-blank.php
+++ wp-content/themes/nutsclub/header-blank.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
-    <link rel="icon" type="image/png" href="<?php echo esc_url(get_template_directory_uri() . '/assets/img/favicon.png'); ?>">
+    <link rel="icon" type="image/png" href="<?php echo esc_url(get_template_directory_uri() . '/assets/img/favicon.png?v=' . filemtime(get_template_directory() . '/assets/img/favicon.png')); ?>">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class('lp-body'); ?>>
